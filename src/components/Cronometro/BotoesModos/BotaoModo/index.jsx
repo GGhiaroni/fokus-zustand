83b1ef689234b7345c32c932e8f87feda3709acb
GGhiaroni <gghiaronitiziano@gmail.com>
2025-03-24 08:16:22 -0300

    implementando a selecao de modo dos botoes da aplicacao com os estados globais

--- src/components/Cronometro/BotoesModos/BotaoModo/index.jsx
+++ src/components/Cronometro/BotoesModos/BotaoModo/index.jsx
@@ -1,11 +1,22 @@
+import { useCronometroStore } from "../../../../store";
 import styles from "./styles.module.css";
 export default function BotaoModo({ children, modoBotao }) {
+  const modoCronometro = useCronometroStore((estado) => estado.modoCronometro);
+  const setModoCronometro = useCronometroStore((estado) => estado.setModoCronometro);
+
+  const botaoAtivo = modoBotao.id === modoCronometro.id;
+
+  const aoClicar = () => {
+    setModoCronometro(modoBotao);
+  };
+
   return (
     <button
       className={`
         ${styles["cronometer-modes__button"]}
-        ${styles["cronometer-modes__button--active"]}
+        ${botaoAtivo ? styles["cronometer-modes__button--active"] : ""}
       `}
+      onClick={aoClicar}
     >
       {children}
     </button>
