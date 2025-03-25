3ea7a1d175b37197b24631d3c3bc227eddeb7552
GGhiaroni <gghiaronitiziano@gmail.com>
2025-03-25 08:30:43 -0300

    inserindo som de tempo finalizado

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,7 @@
 import { create } from "zustand";
+import audioTempoFinalizadoSom from "/src/assets/sons/beep.mp3";
+
+const audioTempoFinalizado = new Audio(audioTempoFinalizadoSom);
 
 export const MODO_CRONOMETRO = {
   FOCO: {
@@ -56,6 +59,7 @@ function computarContagemRegressiva() {
   } else {
     pausarCronometro();
     redefinirTempo();
+    audioTempoFinalizado.play();
   }
 }
 
